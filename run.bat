fab58deac4aeb139af1e0b1cfad681d3fa84e5e2
马智杰 <903448239@qq.com>
2018-01-06 15:05:53 +0800

    .

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 adb push ./Jump.jar /data/local/tmp
-adb shell uiautomator runtest Jump.jar -c dowork.Jump
+adb shell uiautomator runtest Jump.jar -c com.xing.jump.Jump -e defult true -e jumpTime 500
